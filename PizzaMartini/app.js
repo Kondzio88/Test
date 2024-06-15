4f4e2640145a32a3b24d87637b2f3f1cbba16b87
Kondzio88 <115737577+Kondzio88@users.noreply.github.com>
2024-06-15 13:23:06 +0200

    Add files via upload

--- PizzaMartini/app.js
+++ PizzaMartini/app.js
@@ -4,10 +4,8 @@ let login = document.querySelector('.login-form')
 let userBtn = document.querySelector('#login')
 let card = document.querySelector('.shopping-cart')
 
-
-const menuBtn =document.querySelector('#menu')
+const menuBtn = document.querySelector('#menu')
 const nav = document.querySelector('.nav-bar')
-
 
 cart.addEventListener('click', () => {
 	card.classList.toggle('active')
@@ -25,6 +23,19 @@ window.onscroll = () => {
 	nav.classList.remove('active')
 }
 
+// date inner html for blogs secction
+
+const dateEl = document.querySelectorAll('.date')
+
+let year = new Date().getUTCFullYear()
+let day = new Date().getDate()
+let month = new Date().getMonth() + 1
+
+console.log(day)
+dateEl.forEach(x => {
+	x.innerHTML = `<i class="fas fa-calendar"></i>${day}.${month}.${year}`
+})
+
 //  swipper js
 
 var swiper = new Swiper('.home-slider', {
@@ -41,16 +52,8 @@ var swiper = new Swiper('.home-slider', {
 	},
 })
 
-menuBtn.addEventListener('click',()=>{
+menuBtn.addEventListener('click', () => {
 	nav.classList.toggle('active')
 	login.classList.remove('active')
 	card.classList.remove('active')
 })
-
-
-
-let day = new Date().getDate()
-let month =new Date().getMonth() + 1
-let year = new Date().getUTCFullYear()
-
-console.log(year);
